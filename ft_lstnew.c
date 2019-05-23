fa4c2b42e10cd0d777f7d0eb2ca005fec0c7abac
Sunah KIM <kosah302@gmail.com>
2019-05-23 14:39:44 +0200

    ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: sunakim <sunakim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 22:12:04 by mnishimo          #+#    #+#             */
-/*   Updated: 2019/05/23 14:22:39 by sunakim          ###   ########.fr       */
+/*   Updated: 2019/05/23 14:37:14 by sunakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	if (content == NULL)
 	{
 		ptr->content = NULL;
-		ptr->content_size = content_size;
+		ptr->content_size = 0;
 		ptr->next = NULL;
 	}
 	else
